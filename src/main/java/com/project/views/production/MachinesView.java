ef340632797ff2d1223872bc4c1927778376e03a
Jimmy NIYONKURU <104285083+JimmyNIYONKURU@users.noreply.github.com>
2023-12-29 23:10:49 +0100

    Add files via upload
    machine + stats view

--- src/main/java/com/project/views/production/MachinesView.java
+++ src/main/java/com/project/views/production/MachinesView.java
@@ -1,66 +1,107 @@
 package com.project.views.production;
 
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
-import java.util.ArrayList;
-
-
 import com.project.App;
 import com.project.views.MainLayout;
 import com.vaadin.flow.component.Text;
 import com.vaadin.flow.component.button.Button;
-import com.vaadin.flow.component.charts.model.Pane;
 import com.vaadin.flow.component.notification.Notification;
-import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
 
+
+import java.sql.SQLException;
+
+import java.util.ArrayList;
+import java.util.List;
+
 @PageTitle("View your machines")
 @Route(value = "machines", layout = MainLayout.class)
+public class MachinesView extends VerticalLayout {
 
-public class MachinesView extends HorizontalLayout
-{
-    private ArrayList<HorizontalLayout> MachinesList;
+    private final ArrayList<VerticalLayout> machinesList;
 
-    MachinesView()
-    {
-        //ask for the table of machines
-        MachinesList = new ArrayList<>();
-        try(Statement statement = App.manipDB.myConnection.createStatement()
-        ){
-            ResultSet queryResultSet = statement.executeQuery(
-                "SELECT * FROM MACHINE"
-            );
-            while(!queryResultSet.isLast())
+    public MachinesView() {
+        machinesList = new ArrayList<>();
+
+        try {
+            List<Machine> allMachines = App.manipDB.mymanipmachines.getAllMachines();
+
+            for (Machine machine : allMachines) 
             {
-                queryResultSet.next();
-                Button deleteButton = new Button("Delete machine");
-                String machineRef = queryResultSet.getString("REF");
-                deleteButton.addClickListener(e -> {
-                    try{
-                        App.manipDB.mymanipmachines.deleteMachine(machineRef);
-                        //somehow remove the fucking panel or reload the page
-                        this.remove();
-                    }
-                    catch(SQLException err)
-                    {
-                        Notification.show("Impossible to connect delete button to machine with reference: " + machineRef);
-                    }
-                });
-                HorizontalLayout myLayout = new HorizontalLayout(Alignment.AUTO, new Text(queryResultSet.getString("REF")), deleteButton);
-                MachinesList.add(myLayout);
-            }
-            for(HorizontalLayout h : MachinesList)
-            {
-                this.add(h);
+                createMachineLayout(machine);
             }
 
-        }
-        catch(SQLException err){
+            for (VerticalLayout machineLayout : machinesList) 
+            {
+                this.add(machineLayout);
+            }
+
+        } catch (SQLException err) 
+        {
             err.printStackTrace();
+            Notification.show("Error retrieving machine data");
         }
     }
 
+    private void createMachineLayout(Machine machine) 
+    {
+        String machineRef = machine.getRef();
+        String machineState = machine.getState();
 
+        // Avoid the NullPointerException
+        if (machineState != null) {
+            Button deleteButton = new Button("Delete machine");
+            Button changeStateButton = new Button("Change State");
+
+            deleteButton.addClickListener(e -> {
+                try {
+                    App.manipDB.mymanipmachines.deleteMachine(machineRef);
+                    // Remove the layout from the UI
+                    machinesList.removeIf(layout -> layout.getComponentAt(0).getClass().equals(Text.class)
+                            && ((Text) layout.getComponentAt(0)).getText().equals("Reference: " + machineRef));
+                    this.remove(deleteButton.getParent().get());
+                } catch (SQLException err) {
+                    Notification.show("Unable to delete machine with reference: " + machineRef);
+                }
+            });
+        
+            changeStateButton.addClickListener(e -> {
+                // change state in db
+                try {
+                    App.manipDB.mymanipmachines.changeMachineState(machineRef, "New State");
+                    Notification.show("Machine state changed to New State");
+                    // Refresh UI
+                    createMachineLayout(machine);
+                } catch (SQLException err) {
+                    Notification.show("Unable to change state for machine with reference: " + machineRef);
+                }
+            });
+        
+            VerticalLayout machineLayout = new VerticalLayout(
+                new Text("Reference: " + machineRef),
+                new Text("Description: " + machine.getDescription()),
+                new Text("Power: " + machine.getPower()),
+                new Text("State: " + machineState),
+                changeStateButton,
+                deleteButton
+            );
+        
+            switch (machineState) {
+                case "Available":
+                    machineLayout.getStyle().set("background-color", "green");
+                    break;
+                case "Idle":
+                    machineLayout.getStyle().set("background-color", "yellow");
+                    break;
+                case "In Maintenance":
+                    machineLayout.getStyle().set("background-color", "red");
+                    break;
+                default:
+                    machineLayout.getStyle().set("background-color", "white");
+            }
+        
+            machinesList.add(machineLayout);
+        }
+    }
 }
