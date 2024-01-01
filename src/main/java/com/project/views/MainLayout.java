820d683a11dcc68980b7a8a5630cba8d78ccf07b
MahdiMounzer <128136856+MahdiMounzer@users.noreply.github.com>
2024-01-01 17:41:35 +0100

    Update MainLayout.java

--- src/main/java/com/project/views/MainLayout.java
+++ src/main/java/com/project/views/MainLayout.java
@@ -2,6 +2,7 @@ package com.project.views;
 
 import com.project.views.about.AboutView;
 import com.project.views.helloworld.HelloWorldView;
+import com.project.views.production.EconomyView;
 import com.project.views.production.MachinesView;
 
 import com.vaadin.flow.component.applayout.AppLayout;
@@ -58,6 +59,7 @@ public class MainLayout extends AppLayout {
         nav.addItem(new SideNavItem("Hello World", HelloWorldView.class, LineAwesomeIcon.GLOBE_SOLID.create()));
         nav.addItem(new SideNavItem("About", AboutView.class, LineAwesomeIcon.FILE.create()));
         nav.addItem(new SideNavItem("Machines", MachinesView.class, LineAwesomeIcon.COG_SOLID.create()));
+        nav.addItem(new SideNavItem("Economy", EconomyView.class, LineAwesomeIcon.COINS_SOLID.create()));
 
         return nav;
     }
